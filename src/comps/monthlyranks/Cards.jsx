3fd3e96827ad5bd6580ae4e4c784983a8ab4f34a
infodavid00 <da40au40@gmail.com>
2024-08-03 21:36:25 +0100

    updated monthlyranks, removed card images and changed card btn title

--- src/comps/monthlyranks/Cards.jsx
+++ src/comps/monthlyranks/Cards.jsx
@@ -6,111 +6,51 @@ export default function Cards() {
       title: "Abstract Colors",
       name: "Jane Doe",
       digit: "0.90 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-      ],
     },
     {
       title: "Vintage Home",
       name: "Alice Smith",
       digit: "1.25 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-      ],
     },
     {
       title: "Modern Art",
       name: "Bob Johnson",
       digit: "2.10 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-      ],
     },
     {
       title: "Sunset Views",
       name: "Carol White",
       digit: "0.75 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-      ],
     },
     {
       title: "City Lights",
       name: "David Brown",
       digit: "1.85 ETH",
-      images: [
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-      ],
     },
     {
       title: "Ocean Breeze",
       name: "Emma Davis",
       digit: "1.00 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-      ],
     },
     {
       title: "Dreamy Forest",
       name: "Frank Wilson",
       digit: "0.65 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-      ],
     },
     {
       title: "Starry Night",
       name: "Grace Lee",
       digit: "2.50 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-      ],
     },
     {
       title: "Futuristic Vision",
       name: "Hannah Martinez",
       digit: "3.00 ETH",
-      images: [
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-      ],
     },
     {
       title: "Modern Art",
       name: "Bob Johnson",
       digit: "2.10 ETH",
-      images: [
-        "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1780&q=80",
-        "https://i.ibb.co/7p0d1Cd/Frame-24.png",
-        "https://images.unsplash.com/photo-1506863530036-1efeddceb993?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2244&q=80",
-        "https://images.unsplash.com/photo-1573766064535-6d5d4e62bf9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1315&q=80",
-      ],
     },
   ];
 
@@ -118,36 +58,30 @@ export default function Cards() {
     <div id="monthlyranks-cards-body">
       {cards.map((element, index) => (
         <div key={index} className="monthlyranks-card">
-          <h1 className="monthlyranks-card-title">{element.title}</h1>
-          <div className="monthlyranks-card-name">By {element.name}</div>
-          <div className="monthlyranks-card-catalog">
-            <img
-              src={element.images[0]}
-              style={{ marginRight: "-7px", zIndex: 3 }}
-            />
-            <img
-              src={element.images[1]}
-              style={{ marginRight: "-7px", zIndex: 2 }}
-            />
-            <img
-              src={element.images[2]}
-              style={{ marginRight: "-7px", zIndex: 1 }}
-            />
-            <img
-              src={element.images[3]}
-              style={{ marginRight: "-7px", zIndex: 0 }}
-            />
+          <div className="monthlyranks-card-index">
+            <div className="monthlyranks-card-index-btn">{index + 1}</div>
           </div>
-          {/* section1 */}
-          <div style={{ marginTop: "1em" }}>
-            <div className="monthlyranks-card-footer-text">
-              Current Bid: {element.digit}
+          <div className="monthlyranks-card-padded">
+            <h1 className="monthlyranks-card-title">{element.title}</h1>
+            <div className="monthlyranks-card-name">By {element.name}</div>
+            {/* section1 */}
+            <div style={{ marginTop: "1em" }}>
+              <div className="monthlyranks-card-footer-text">
+                Current Bid: {element.digit}
+              </div>
+              <button className="monthlyranks-card-footer-btn">Swap</button>
             </div>
-            <button className="monthlyranks-card-footer-btn"> Place Bid</button>
+            {/* section2 */}
           </div>
-          {/* section2 */}
         </div>
       ))}
     </div>
   );
 }
+
+//  <div className="monthlyranks-card-catalog">
+//    <img src={element.images[0]} style={{ marginRight: "-7px", zIndex: 3 }} />
+//    <img src={element.images[1]} style={{ marginRight: "-7px", zIndex: 2 }} />
+//    <img src={element.images[2]} style={{ marginRight: "-7px", zIndex: 1 }} />
+//    <img src={element.images[3]} style={{ marginRight: "-7px", zIndex: 0 }} />
+//  </div>;
