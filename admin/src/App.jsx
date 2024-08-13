11281deec9d3b8156252c1b8256ef1d5d9af2705
infodavid00 <da40au40@gmail.com>
2024-08-13 18:12:16 +0100

    added signout

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import Logout from "./pages/Logout";
 import Support from "./pages/Support";
 import News from "./pages/News";
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/logout" element={<Logout />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/dashboard/support" element={<Support />} />
         <Route exact path="/dashboard/news" element={<News />} />
---
+++ admin/src/pages/Logout.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import verify from "../etc/verify.jsx";
+import { tokenVault, BaseEndpoint } from "../etc/network.jsx";
+import Cookies from 'js-cookie';
+import { TailSpin } from 'react-loader-spinner'; // Assuming you're using TailSpin from this package
+
+export default function Logout() {
+  const [isVerified, setIsVerified] = useState(false);
+
+  useEffect(() => {
+    verify(setIsVerified);
+  }, []);
+
+  useEffect(() => {
+    if (isVerified) {
+      const TOKEN = Cookies.get(tokenVault);
+      if (TOKEN) {
+        fetch(`${BaseEndpoint}/auth/signout`, {
+          method: 'POST',
+          headers: {
+            'Pass': `${btoa(TOKEN)}`,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(response => {
+          if (response.ok) {
+            window.location.href = "/login";
+            Cookies.remove(tokenVault);
+          }
+        })
+        .catch((error) => {
+          return ""
+        });
+      } else {
+        window.location.href = "/login";
+      }
+    }
+  }, [isVerified]);
+
+  if (!isVerified) {
+    return (
+      <div id="loader-container">
+        <TailSpin height="20" width="20" color="#fff" />
+      </div>
+    );
+  }
+
+  return null;
+}
