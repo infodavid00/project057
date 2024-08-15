a4e974838ddfc02378b9ce9777ad69ad0cf43d3c
infodavid00 <da40au40@gmail.com>
2024-08-15 10:21:27 +0100

    changed sync interval

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ import sroot from "./sroute/root.js";
 import { dbConnect } from "./store/connections.js";
 import removeLogsFromInvitePool from "./etc/removeLogsFromInvitePool.js";
 import sync from "./kernel/reports/sync.js";
+import cloudinary from './etc/cloudinary.js';
 
 dotenv.config();
 
@@ -20,6 +21,25 @@ app.use(express.json());
 app.use("/u", root);
 app.use("/su", sroot);
 
+app.use('/upload', 
+  express.json({ limit: '10mb' }), 
+  express.urlencoded({ extended: true })
+);
+app.post('/upload/image', async (req, res) => {
+  try {
+    const { image } = req.body;
+    const result = await cloudinary.uploader.upload(`data:image/jpeg;base64,${image}`);
+    res.status(200).json({
+      message: 'Image uploaded successfully!',
+      url: result.secure_url,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to upload image.' });
+  }
+});
+
+
 dbConnect(process.env.DBKEY)
   .connect()
   .then(() => {
@@ -29,8 +49,8 @@ dbConnect(process.env.DBKEY)
       //appx 27 hours interval the +3 hours is to ensure some records created close
       //to the next day eg an hour or few minutes to the next day, are not 
       //deleted prematurely.
-      setInterval(async () => await sync(), 10000)
-      // appx after every 20 seconds: should be 30 mins on prod
+      setInterval(async () => await sync(), 600000)
+      // appx after every 600k ms (10 minutes)
     });
   })
   .catch((error) => {
