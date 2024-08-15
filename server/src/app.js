0c7756b6870bceddd471788fc6ec3a75393c5f4a
infodavid00 <da40au40@gmail.com>
2024-08-15 10:45:13 +0100

    removed cloudinary

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,6 @@ import sroot from "./sroute/root.js";
 import { dbConnect } from "./store/connections.js";
 import removeLogsFromInvitePool from "./etc/removeLogsFromInvitePool.js";
 import sync from "./kernel/reports/sync.js";
-import cloudinary from './etc/cloudinary.js';
 
 dotenv.config();
 
@@ -20,25 +19,6 @@ app.use(express.json());
 
 app.use("/u", root);
 app.use("/su", sroot);
-
-app.use('/upload', 
-  express.json({ limit: '10mb' }), 
-  express.urlencoded({ extended: true })
-);
-app.post('/upload/image', async (req, res) => {
-  try {
-    const { image } = req.body;
-    const result = await cloudinary.uploader.upload(`data:image/jpeg;base64,${image}`);
-    res.status(200).json({
-      message: 'Image uploaded successfully!',
-      url: result.secure_url,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to upload image.' });
-  }
-});
-
 
 dbConnect(process.env.DBKEY)
   .connect()
