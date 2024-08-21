edb1dd8e2715f578900c30e06d29fd181b2800b9
infodavid00 <da40au40@gmail.com>
2024-08-21 14:30:28 +0100

    latest

--- client/src/comps/dashboard/Card.jsx
+++
@@ -1,8 +0,0 @@
-import "./dashboard.css";
-
-function Card(props) {
-  const { children } = props;
-  return <div className="dashboard-charts-box">{children}</div>;
-}
-
-export default Card;
--- client/src/theme.jsx
+++ client/src/theme.jsx
@@ -4,7 +4,7 @@ const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: "var(--background-color)", // Set your background color here
+        bg: "var(--background-color)", 
       },
     },
   },
